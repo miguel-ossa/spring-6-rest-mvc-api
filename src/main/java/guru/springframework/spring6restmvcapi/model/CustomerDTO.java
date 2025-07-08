8a13de9e045771745c255a293797bbe36efb8320
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-07-08 12:05:15 +0100

    Some Fixes

--- src/main/java/guru/springframework/spring6restmvcapi/model/CustomerDTO.java
+++ src/main/java/guru/springframework/spring6restmvcapi/model/CustomerDTO.java
@@ -1,7 +1,6 @@
 package guru.springframework.spring6restmvcapi.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
